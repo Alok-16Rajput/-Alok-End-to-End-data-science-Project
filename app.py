8f9e8646b6553a88479ca439c8c03b400933e112
“[firstname <[cmalokkumar@gmail.com]>
2025-01-24 13:42:00 +0530

    Data ingestion

---
+++ app.py
@@ -0,0 +1,17 @@
+from src.Datascienceproject.logger import logging
+from src.Datascienceproject.exception import CustomException
+from src.Datascienceproject.components.data_ingestion import DataIngestion
+from src.Datascienceproject.components.data_ingestion import DataIngestionConfig
+import sys
+
+
+if __name__=="__main__":
+    logging.info("The execution has started")
+     
+    try:
+       # data_ingestion_config=DataIngestionConfig()
+        data_ingestion=DataIngestion()
+        data_ingestion.initiate_data_ingestion()
+    except Exception as e:
+        logging.info("Custom Exception")
+        raise CustomException(e,sys)
